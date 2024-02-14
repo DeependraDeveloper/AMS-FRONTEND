6ca8e352adbd79dc59e96b0f7504a23c133cdfac
Ravi Kovind <111339375+DeependraDDEV@users.noreply.github.com>
2024-02-14 13:13:22 +0530

    added maps

--- lib/views/home/clock.dart
+++ lib/views/home/clock.dart
@@ -53,29 +53,22 @@ class _ClockState extends State<Clock> {
     // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
+      // request permissions until the user grants them
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
     }
 
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
+        // request permissions until the user grants them
+        permission = await Geolocator.requestPermission();
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
+      // request permissions until the user grants them
+      permission = await Geolocator.requestPermission();
     }
 
     // When we reach here, permissions are granted and we can
@@ -131,6 +124,138 @@ class _ClockState extends State<Clock> {
     final bool isOutTimeEmpty =
         showClock.outTime == null || showClock.outTime == '';
 
+    return GestureDetector(
+      onTap: () async {
+        final Position position = await _determinePosition();
+
+        await showDialog(
+          context: context,
+          builder: (context) {
+            return MapDisplay(
+              latitude: position.latitude,
+              longitude: position.longitude,
+            );
+          },
+        );
+
+        if (isInTimeEmpty && !isClockedIn) {
+          startTimer();
+          isClockedIn = true;
+        }
+      },
+      child: Container(
+        width: 154,
+        height: 52,
+        decoration: BoxDecoration(
+          color: isInTimeEmpty
+              ? Colors.red
+              : (isOutTimeEmpty ? Colors.blue : Colors.red),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: FittedBox(
+          child: isInTimeEmpty
+              ? Column(
+                  children: [
+                    Text(
+                      newDt,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                    const Text(
+                      'Clock In',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                )
+              : (isOutTimeEmpty
+                  ? Column(
+                      children: [
+                        Text(
+                          newDt,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            letterSpacing: 1.5,
+                          ),
+                        ),
+                        Text(
+                          _currentTime,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            letterSpacing: 1.5,
+                          ),
+                        ),
+                      ],
+                    )
+                  : Column(
+                      children: [
+                        Text(
+                          newDt,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            letterSpacing: 1.5,
+                          ),
+                        ),
+                        const Text(
+                          'Clock In',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    )),
+        ),
+      ),
+    );
+  }
+}
+
+class MapDisplay extends StatelessWidget {
+  const MapDisplay({
+    super.key,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  final double latitude;
+  final double longitude;
+
+  String getTime() {
+    final DateTime now = DateTime.now();
+    int hour = now.hour;
+    String period = 'AM';
+
+    if (hour >= 12) {
+      period = 'PM';
+      if (hour > 12) {
+        hour -= 12;
+      }
+    }
+
+    final String formattedHour = hour.toString().padLeft(2, '0');
+    final String minute = now.minute.toString().padLeft(2, '0');
+    final String second = now.second.toString().padLeft(2, '0');
+
+    return '$formattedHour:$minute:$second $period';
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return BlocConsumer<UserBloc, UserState>(
       listener: (context, state) {
         if (state.error.isNotEmpty) {
@@ -141,12 +266,12 @@ class _ClockState extends State<Clock> {
             ),
           );
         } else if (state.message.isNotEmpty) {
-          // ScaffoldMessenger.of(context).showSnackBar(
-          //   SnackBar(
-          //     content: Text(state.message),
-          //     backgroundColor: Colors.green,
-          //   ),
-          // );
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.message),
+              backgroundColor: Colors.green,
+            ),
+          );
         }
       },
       builder: (context, state) {
@@ -155,203 +280,57 @@ class _ClockState extends State<Clock> {
             child: CircularProgressIndicator(),
           );
         }
-
-        return GestureDetector(
-          onTap: () async {
-            // const UserMapInfo();
-
-            final String id = context.read<AuthBloc>().state.user.id ?? '';
-            final String time = getTime();
-            context.read<UserBloc>().add(ClockInOutEvent(id: id, time: time));
-
-            if (isInTimeEmpty && !isClockedIn) {
-              startTimer();
-              isClockedIn = true;
-            }
-          },
-          child: Container(
-            width: 154,
-            height: 52,
+        return AlertDialog(
+          contentPadding: const EdgeInsets.all(8.0),
+          title: const Text('Your Location'),
+          content: Container(
             decoration: BoxDecoration(
-              color: isInTimeEmpty
-                  ? Colors.red
-                  : (isOutTimeEmpty ? Colors.blue : Colors.red),
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(4.0),
+              border: Border.all(color: Colors.blueAccent, width: 2.0),
             ),
-            child: FittedBox(
-                child: isInTimeEmpty
-                    ? Column(
-                        children: [
-                          Text(
-                            newDt,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white,
-                              letterSpacing: 1.5,
-                            ),
-                          ),
-                          const Text(
-                            'Clock In',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      )
-                    : (isOutTimeEmpty
-                        ? Column(
-                            children: [
-                              Text(
-                                newDt,
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white,
-                                  letterSpacing: 1.5,
-                                ),
-                              ),
-                              Text(
-                                _currentTime,
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white,
-                                  letterSpacing: 1.5,
-                                ),
-                              ),
-                            ],
-                          )
-                        : Column(
-                            children: [
-                              Text(
-                                newDt,
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white,
-                                  letterSpacing: 1.5,
-                                ),
-                              ),
-                              const Text(
-                                'Clock In',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ))),
+            width: MediaQuery.of(context).size.width,
+            height: 500,
+            child: FlutterMap(
+              options: MapOptions(
+                initialCenter: LatLng(latitude, longitude),
+                initialZoom: 18.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  maxZoom: 19,
+                ),
+                LocationMarkerLayer(
+                  position: LocationMarkerPosition(
+                    latitude: latitude,
+                    longitude: longitude,
+                    accuracy: 9.0,
+                  ),
+                ),
+              ],
+            ),
           ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                final String id = context.read<AuthBloc>().state.user.id ?? '';
+                final String time = getTime();
+                context
+                    .read<UserBloc>()
+                    .add(ClockInOutEvent(id: id, time: time));
+                Navigator.pop(context);
+              },
+              child: const Text('Proceed'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
         );
       },
-    );
-  }
-}
-
-// class UserMapInfo extends StatefulWidget {
-//   const UserMapInfo({super.key});
-
-//   @override
-//   State<UserMapInfo> createState() => _UserMapInfoState();
-// }
-
-// class _UserMapInfoState extends State<UserMapInfo> {
-//   late GoogleMapController mapController;
-
-//   LatLng? _currentPosition;
-//   bool _isLoading = true;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     getLocation();
-//   }
-
-//   getLocation() async {
-//     LocationPermission permission;
-
-//     permission = await Geolocator.requestPermission();
-
-//     Position position = await Geolocator.getCurrentPosition(
-//         desiredAccuracy: LocationAccuracy.high);
-//     double lat = position.latitude;
-//     double long = position.longitude;
-
-//     LatLng location = LatLng(lat, long);
-
-//     setState(() {
-//       _currentPosition = location;
-//       _isLoading = false;
-//     });
-//   }
-
-//   void _onMapCreated(GoogleMapController controller) {
-//     mapController = controller;
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Map'),
-//       ),
-//       body: _isLoading
-//           ? const Center(child: CircularProgressIndicator())
-//           : GoogleMap(
-//               onMapCreated: _onMapCreated,
-//               initialCameraPosition: CameraPosition(
-//                 target: _currentPosition ?? const LatLng(0, 0),
-//                 zoom: 16.0,
-//               ),
-//             ),
-//     );
-//   }
-// }
-
-class MinimumExample extends StatelessWidget {
-  const MinimumExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4.0),
-                border: Border.all(color: Colors.blueAccent, width: 2.0),
-              ),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 2,
-              child: FlutterMap(
-                options: const MapOptions(
-                  initialCenter: LatLng(13.1051344, 77.577344),
-                  initialZoom: 18.0,
-                ),
-                children: [
-                  TileLayer(
-                    urlTemplate:
-                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    maxZoom: 19,
-                  ),
-                  LocationMarkerLayer(
-                    position: LocationMarkerPosition(
-                        latitude: 13.1051344,
-                        longitude: 77.577344,
-                        accuracy: 9.0),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
